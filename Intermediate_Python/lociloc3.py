175dbd44aa8588d980d769f87cd47b6a4ace51fb
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-02-05 02:08:21 +0530

    loc and iloc (3)

---
+++ Intermediate_Python/lociloc3.py
@@ -0,0 +1,13 @@
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out drives_right column as Series
+print(cars['drives_right'])
+
+# Print out drives_right column as DataFrame
+print(cars[['drives_right']])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars[['cars_per_cap', 'drives_right']])
+
