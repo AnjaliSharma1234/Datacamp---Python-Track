e242487ae843656a4b05a1368c590747ba3be6ad
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-02-09 02:01:35 +0530

    Functions that return single values

---
+++ Python_DataScience_ToolBox(1)/FunctionsReturningSingleValue.py
@@ -0,0 +1,15 @@
+# Define shout with the parameter, word
+def shout(word):
+    """Return a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + '!!!'
+    return shout_word
+
+    # Replace print with return
+    print(shout_word)
+
+# Pass 'congratulations' to shout: yell
+yell = shout('congratulations')
+
+# Print yell
+print(yell)
