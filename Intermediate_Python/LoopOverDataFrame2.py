8828789fa369fe8086258e31591fb68684e87712
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-02-08 13:03:51 +0530

    Loop over DataFrame (2)

---
+++ Intermediate_Python/LoopOverDataFrame2.py
@@ -0,0 +1,8 @@
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Adapt for loop
+for lab, row in cars.iterrows() :
+    print(lab + ": " + str(row['cars_per_cap']))
+
