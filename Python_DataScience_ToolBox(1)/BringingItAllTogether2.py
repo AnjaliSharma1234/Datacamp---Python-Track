552f6dd0d6b85db64dc97c6df18e5ee84180a6c9
Anjali Sharma <52730168+AnjaliSharma1234@users.noreply.github.com>
2020-02-10 01:46:53 +0530

    Bringing it all together (2)

--- Python_DataScience_ToolBox(1)/BringingItAllTogether2.py
+++ Python_DataScience_ToolBox(1)/BringingItAllTogether2.py
@@ -1,29 +1,37 @@
 # Define count_entries()
-def count_entries(df, col_name):
-    """Return a dictionary with counts of 
+def count_entries(df, *args):
+    """Return a dictionary with counts of
     occurrences as value for each key."""
-
-    # Initialize an empty dictionary: langs_count
-    langs_count = {}
     
-    # Extract column from DataFrame: col
-    col = df[col_name]
+    #Initialize an empty dictionary: cols_count
+    cols_count = {}
     
-    # Iterate over lang column in DataFrame
-    for entry in col:
+    # Iterate over column names in args
+    for col_name in args:
+    
+        # Extract column from DataFrame: col
+        col = df[col_name]
+    
+        # Iterate over the column in DataFrame
+        for entry in col:
+    
+            # If entry is in cols_count, add 1
+            if entry in cols_count.keys():
+                cols_count[entry] += 1
+    
+            # Else add the entry to cols_count, set the value to 1
+            else:
+                cols_count[entry] = 1
 
-        # If the language is in langs_count, add 1
-        if entry in langs_count.keys():
-            langs_count[entry] += 1
-        # Else add the language to langs_count, set the value to 1
-        else:
-            langs_count[entry] = 1
+    # Return the cols_count dictionary
+    return cols_count
 
-    # Return the langs_count dictionary
-    return langs_count
+# Call count_entries(): result1
+result1 = count_entries(tweets_df, 'lang')
 
-# Call count_entries(): result
-result = count_entries(tweets_df,'lang')
+# Call count_entries(): result2
+result2 = count_entries(tweets_df, 'lang', 'source')
 
-# Print the result
-print(result)
+# Print result1 and result2
+print(result1)
+print(result2)
